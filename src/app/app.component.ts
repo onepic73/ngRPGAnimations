c9bc4a71d90704d19d453067375d578ff6917d57
Mathieu Briau <mathieu.briau@gmail.com>
2024-03-25 17:26:57 -0400

    Mort de Slimey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
+import {shakeX} from "ng-animate";
 
 // Même durée que l'animation de FadeIn
 const SPAWN_DURATION_MS = 500;
 
+const DEATH_DURATION_SECONDS = 0.5;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('death', [transition(':increment', useAnimation(shakeX, {params: {timing: DEATH_DURATION_SECONDS}}))]),
+  ]
+  
 })
 export class AppComponent {
   slimeIsPresent = false;
   cantInteractWithSlime = false;
+
+  ng_death = 0;
   
   constructor() {
   }
@@ -20,6 +29,12 @@ export class AppComponent {
     var element = document.getElementById("slimeyId");
     element?.classList.remove("fadeOut");
     element?.classList.add("fadeIn");
+  }
+
+  hideSlime(){
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeIn");
+    element?.classList.add("fadeOut");
   }
 
   spawn() {
@@ -31,9 +46,9 @@ export class AppComponent {
   death(){
     this.slimeIsPresent = false;
     // TODO Cacher le slime
-    
+    this.hideSlime();
     // TODO Déclencher une animation angular pour la mort
-    
+    this.ng_death++;
     // TODO [Plus tard] Désactiver les boutons pendant que l'animation roule
   }
 
