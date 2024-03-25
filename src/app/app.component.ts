b89133285344fff64bc72b68d28f9be6f686f672
Mathieu Briau <mathieu.briau@gmail.com>
2024-03-25 14:52:38 -0400

    Ajout de plusieurs animations, incluant un wobble pas encore fonctionnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,9 @@ const FLEE_DURATION_MS = FLEE_DURATION_SECONDS * 1000;
   styleUrls: ['./app.component.css'],
   animations:[
     trigger('shake', [transition(':increment', useAnimation(shakeX, {params: {timing: DEATH_DURATION_SECONDS}}))]),
-    trigger('jello', [transition(':increment', useAnimation(rubberBand, {params: {timing: 2}}))]),
+    trigger('jello', [
+      transition(':increment', useAnimation(jello, {params: {timing: 0.5}}))
+    ]),
     trigger('tada', [transition(':increment', useAnimation(pulse, {params: {timing: 0.5, scale: 2.5}}))]),
     trigger('backOutRight', [transition(':increment', useAnimation(backOutRight, {params: {timing: FLEE_DURATION_SECONDS}}))]),
     trigger('enter', [transition(':increment', useAnimation(jello, {params: {timing: 10}}))]),
@@ -47,110 +49,9 @@ export class AppComponent {
   css_bounce = false;
   css_flip = false;
 
+  css_hit = false;
+
   constructor() {
-  }
-
-  ngShakeMeInfinite() {
-    this.ng_shake++;
-    setTimeout(() => {
-      this.ngBounceMeInfinite();
-    },2000);
-  }
-
-  ngBounceMeInfinite() {
-    this.ng_bounce++;
-    setTimeout(() => {
-      this.ngShakeMeInfinite();
-    },2000);
-  }
-
-  shakeMeInfinite() {
-    this.css_shake_infinite = true;
-    setTimeout(() => {
-      this.css_shake_infinite = false;
-      this.bounceMeInfinite();
-    },2000);
-  }
-
-  bounceMeInfinite() {
-    this.css_bounce_infinite = true;
-    setTimeout(() => {
-      this.css_bounce_infinite = false;
-      this.shakeMeInfinite();
-    },2000);
-  }
-
-
-
-  async shakeAndBounceMeInfinite2() {
-    while(true){
-      this.css_bounce_infinite = true;
-      await lastValueFrom(timer(2000));
-      this.css_bounce_infinite = false;
-      this.css_shake_infinite = true;
-      await lastValueFrom(timer(2000));
-      this.css_shake_infinite = false;
-    }
-  }
-
-  async ngShakeAndBounceMeInfinite2() {
-    while(true){
-      this.ng_bounce++;
-      await lastValueFrom(timer(2000));
-      this.ng_shake++;
-      await lastValueFrom(timer(2000));
-    }
-  }
-
-  shakeMe() {
-    this.css_shake = true;
-    setTimeout(() => {this.css_shake = false;},2000);
-  }
-
-  bounceMe() {
-    this.css_bounce = true;
-    setTimeout(() => {this.css_bounce = false;},2000);
-  }
-
-  flipMe() {
-    this.css_flip = true;
-    setTimeout(() => {this.css_flip = false;},2000);
-  }
-
-  shakeFlipAndBounce1() {
-    this.shakeMe();
-    setTimeout(() => {
-      this.flipMe()
-      setTimeout(() => {
-        this.bounceMe()
-      }, 2000);
-    }, 2000);
-  }
-
-  shakeFlipAndBounce2() {
-    this.css_shake = true;
-
-    setTimeout(() => {
-      this.css_shake = false;
-      this.css_flip = true;
-    },2000);
-
-    setTimeout(() => {
-      this.css_flip = false;
-      this.css_bounce = true;
-    },4000);
-
-    setTimeout(() => {
-      this.css_bounce = false;
-    },6000);
-  }
-
-  async shakeFlipAndBounce3() {
-    this.shakeMe();
-    await lastValueFrom(timer(2000));
-    this.flipMe();
-    await lastValueFrom(timer(2000));
-    this.bounceMe();
   }
 
   waitUntilEndOfAnimation(delay:number){
@@ -175,47 +76,34 @@ export class AppComponent {
   spawn() {
     // Simple showSlime
     this.showSlime();
+    // TODO: Montrer l'importance de forwards!
     this.waitUntilEndOfAnimation(SPAWN_DURATION_MS);
   }
 
   death(){
-    // Ajout d'une animation angular
     this.hideSlime();
+    // Ajout d'une animation angular
     this.ng_shake++;
     this.waitUntilEndOfAnimation(DEATH_DURATION_MS);
   }
 
   flee(){
-    // Garder le blob caché après!
+    // TODO: Garder le blob caché après!
     this.ng_backOutRight++;
     this.slimeIsPresent = false;
     this.waitUntilEndOfAnimation(FLEE_DURATION_MS);
   }
 
-  hit(){
-    this.ng_jello++;
-  }
-
   attack(){
+    // TODO: Augment the intensity of the movement avec scale! (Regarder dans le code!)
     // TODO: Augment the intensity of the movement!
+    // TODO: Jouer deux animaions (une après l'aure!)
     this.ng_tada++;
   }
 
-  burn(){
-    // Find a burning animation!
-    this.ng_burn++;
+  hit(){
+    //Utilisé anamista pour faire une animation différente avec css
+    this.css_hit = true;
+    //setTimeout(() => this.css_hit = false, 1000);
   }
-
-  scaleUp(){
-    // Make it scale!
-    // Utiliser le css pour cette partie (ou une animation manuelle en tout cas)
-  }
-
-  addCrown(){
-    // Make it scale! + Add a Crown!
-  }
-
-  
-
-  // TODO: Make an idle breathing animation?
 }
